7cc0ce0d134d410824f38448f27cf641e30f0287
Nick Santos <nicholas.j.santos@gmail.com>
2020-03-17 18:29:18 -0400

    add an example that measures deployment time

---
+++ 1-measured/src/main/java/dev/tilt/example/IndexController.java
@@ -0,0 +1,39 @@
+package dev.tilt.example;
+
+import java.time.Duration;
+import java.time.Instant;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+  private final long startTimeSecs = 1584484045;
+  private final long startTimeNanos = 704423022;
+  private final String updateDuration;
+
+  public IndexController() {
+    if (startTimeSecs == 0) {
+      updateDuration = "N/A";
+      return;
+    }
+
+    Instant now = Instant.now();
+    Instant startInstant = Instant.ofEpochSecond(startTimeSecs, startTimeNanos);
+    Duration updateDuration = Duration.between(startInstant, now);
+
+    this.updateDuration = String.format("%.1f", updateDuration.toMillis() / 1000.0) + "s";
+    System.out.printf("Deploy time: %s\n", this.updateDuration);
+  }
+
+
+  @GetMapping("/")
+  public String index(Model model) {
+    // Serves the index.html template under
+    // src/main/resources/templates/index.html
+    model.addAttribute("updateDuration", updateDuration);
+    return "index";
+  }
+
+}
