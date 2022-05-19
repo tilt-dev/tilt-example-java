063a070d6c3c2f4b638a16bc77f7ea7e1251adf7
Mario Loriedo <mario.loriedo@gmail.com>
2022-05-19 17:56:31 +0200

    Mattia fixes (#2)
    * Mattia's fixes
    
    * Adding .vscode folder and update gitignore
    
    Signed-off-by: Mario Loriedo <mario.loriedo@gmail.com>

--- 201-quarkus-live-update/src/test/java/dev/tilt/example/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package dev.tilt.example;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
